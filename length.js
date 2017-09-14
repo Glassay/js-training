c831f448e84eceb6d789b1000d283bc10c771c0a
Glassay <31647920+Glassay@users.noreply.github.com>
2017-09-14 14:58:44 +0800

    slice 初步使用

--- length.js
+++ length.js
@@ -7,3 +7,5 @@ arr.length = 6;
 console.log(arr);
 arr.length = 2;
 console.log(arr);
+arr.length = 3;
+console.log(arr);
---
+++ slice.js
@@ -0,0 +1,12 @@
+/*2017-9-14 Jifeng Cheng
+*
+*/
+var arr = [1,2,3,4,5,6,7,3,4,5,1]
+console.log(`arr.slice(1,3) => [${arr.slice(1,3)}]`);
+console.log(`arr.slice(4,8) => [${arr.slice(4,8)}]`);
+console.log(`arr.slice(11) => [${arr.slice(11)}]`);
+console.log(`arr.slice(-2,4) => [${arr.slice(-2,4)}]`);
+console.log(`arr.slice(-3) => [${arr.slice(-3)}]`);
+console.log(`arr.slice(-5) => [${arr.slice(-5)}]`);
+console.log(`arr.slice(-2,-4) => [${arr.slice(-2,-4)}]`);
+console.log(`arr.slice(-3,3) => [${arr.slice(-3,3)}]`);
