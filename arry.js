2d68bdc5bf53db3d46518e6b4960bacaf9fdc8ef
Glassay <31647920+Glassay@users.noreply.github.com>
2017-09-14 15:40:38 +0800

    arry
    push,pop,unshift,shift

--- arry.js
+++ arry.js
@@ -1,22 +1,32 @@
 /* 2017-9-7    Jifeng Cheng
 *
 */
+
 var arr = [1,2,3];
+
 arr.unshift('a','b');
 console.log(`arr.unshift('a','b') => [${arr}]`);
+
 arr.shift('a');
 console.log(`arr.shift('a') => [${arr}]`);
+
 arr.push('q');
 console.log(`arr.push('q') => [${arr}]`);
+
 arr.pop();
 console.log(`arr.pop() => [${arr}]`);
+
 arr.pop();
 console.log(`arr.pop() => [${arr}]`);
+
 arr.pop();
 console.log(`arr.pop() => [${arr}]`);
+
 arr.pop();
 console.log(`arr.pop() => [${arr}]`);
+
 arr.pop();
 console.log(`arr.pop() => [${arr}]`);
+
 arr.pop();
 console.log(`arr.pop() => [${arr}]`);
