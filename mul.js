2ad0d91d39d2a3f57285e540cbe23d0486876b0c
Glassay <31647920+Glassay@users.noreply.github.com>
2017-09-19 18:37:48 +0800

    mul
    multip list

--- mul.js
+++ mul.js
@@ -1,19 +1,20 @@
-for (y = 1;y <= 9;y ++) {     
+/*for (y = 1;y <= 9;y ++) {     
     var line = [''];
     for (x = 1;x <= y;x ++) {
         line.push(`${x*y}`);
     }console.log(`${line}`);
     
-}
-
+}*/
+console.log(`   1 2  3  4  5  6  7  8  9`);
 for (y = 1;y <= 9;y ++) {     
     var line = '';
     for (x = 1;x <= y;x ++) {
         line = line + `${x * y}` + ' ';
 
     }
-    console.log(`${line}`);
-    console.log(`\n`);
+    //console.log('乘法表');
+    console.log(`${x - 1}  ${line}`);
     
 }
+
 
