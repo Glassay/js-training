747cb0fefb4b9bd377bb454d6ab3828a37261553
Glassay <31647920+Glassay@users.noreply.github.com>
2017-09-22 19:45:18 +0800

    collection
    collection method

---
+++ animal/collection.js
@@ -0,0 +1,12 @@
+/**
+ * 2017-9-22 Jifeng Cheng
+ * collection method
+ */
+
+ //filter:
+ var users = [
+    { 'user': 'zhang', 'age': 36, 'active': true },
+    { 'user': 'cheng',   'age': 40, 'active': false }
+  ];
+ console.log('gather:');
+ console.log(_.filter(gather,true));
